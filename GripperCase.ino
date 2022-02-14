d6fdac10459eaf6d248a4e6de2e4d847025f32c7
Mihail Bondarenco <bondarenco.mihail@gmail.com>
2022-02-14 15:50:52 +0100

    Use actual Servo

--- GripperCase.ino
+++ GripperCase.ino
@@ -1,29 +1,37 @@
 #include <Servo.h>
 
 class ServoMG996R{
-  const int port = 3;
+  const int port = A0;
 
   Servo* servo = new Servo();
+  int minSignal = 88;
+  int maxSignal = 97;
+
+  int spinToSignal(int spin){
+    if(spin < 0) return this->maxSignal + abs(spin);
+    else if (spin > 0) return this->minSignal - abs(spin);
+    else return 90;
+  }
   
   public:    
-    int minAngle = 0;
-    int maxAngle = 120;
-    
     ServoMG996R(){
       servo->attach(this->port);
     }
 
-    void Turn(int degreeAngle){
-      this->servo->write(degreeAngle);
+    void Turn(int spin){
+      Serial.print("Turning servo to:");
+      Serial.println(spin);
+      this->servo->write(this->spinToSignal(spin));
+    }
+
+    void Stop(){
+      this->Turn(0);
     }
 };
 
 class PressureSensor{
-    const int dataPort = 4;
+    const int dataPort = A1;
     
-    int minAngle = -60;
-    int maxAngle = 60;
-
   public:
       PressureSensor(){
           pinMode(dataPort, INPUT);
@@ -31,7 +39,11 @@ class PressureSensor{
   
       float GetPressure(){
         // transform pressure
-        return analogRead(this->dataPort);
+        float pressure = analogRead(this->dataPort);
+        
+        Serial.print("Pressure is:");
+        Serial.println(pressure);
+        return pressure;
       }
 };
 
@@ -43,29 +55,34 @@ class Gripper {
     Gripper(){
       this->servo = new ServoMG996R();
       this->pressure = new PressureSensor();
+      this->servo->Turn(-10);
+      delay(1000);
+      this->servo->Stop();
     }
 
     void Pick(){
-      int angle = this->servo->minAngle;
+      int spin = 0;
       while(this->pressureNotReadyToPick()){
-        this->servo->Turn(angle);
-        angle++;
+        this->servo->Turn(spin);
+        spin++;
       }
+      this->servo->Stop();
     }
 
     void Release(){
       while(this->pressureNotReadyToRelease()){
-        this->servo->Turn(this->servo->minAngle);
+        this->servo->Turn(-10);
       }
+      this->servo->Stop();
     }
 
   private:
     bool pressureNotReadyToPick(){
-      return this->pressure->GetPressure() < 10;
+      return this->pressure->GetPressure() < 500;
     }
 
     bool pressureNotReadyToRelease(){
-      return this->pressure->GetPressure() >= 1;
+      return this->pressure->GetPressure() >= 500;
     }
 };
 
@@ -78,16 +95,31 @@ void setup(){
   pinMode(PICKING_PIN, INPUT);
   pinMode(BUSY_PIN, OUTPUT);
   gripper = new Gripper();
+
+  Serial.begin(9600);
 }
 
 void loop(){
-  if (digitalRead(PICKING_PIN) == HIGH) {
+  Serial.read();
+  Serial.println("Waiting for command");
+  
+  while(Serial.available() == 0){}
+  Serial.println("Command is:");
+
+  int command = Serial.parseInt();
+  Serial.println(command);
+  
+  if (command) {
+    Serial.println("Picking");
     digitalWrite(BUSY_PIN, HIGH);
     gripper->Pick();
     digitalWrite(BUSY_PIN, LOW);
+    Serial.println("Finished Picking");
   } else {
+    Serial.println("Releasing");
     digitalWrite(BUSY_PIN, HIGH);
     gripper->Release();
     digitalWrite(BUSY_PIN, LOW);
+    Serial.println("Finished Releasing");
   }
 }
